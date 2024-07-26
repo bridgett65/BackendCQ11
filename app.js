04b0eeef8ffb94de12023e95644ac26dc144842c
Bridgett65 <nagashabridget8@gmail.com>
2024-07-26 11:28:04 +0300

    connected mongodb to vscode

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express=require('express')
+const express=require('express');
+const mongoose=require('mongoose');
 
-const app=express();
+const app=express();//creating an express app
 const port=4000
 
 app.get('/',(req,res)=>{
-  res.send('we are code queens')
+
 })
 
-app.listen(port)
+//CONNECT TO THE DATABASE
+mongoose.connect ("mongodb+srv://Bridget65:momlove65@bridget.tfhzypk.mongodb.net/?retryWrites=true&w=majority&appName=Bridget",{userNewUrlParser:true})
+  //parsing error
+.then(() =>{
+  console.log('connected to the DB');
+})
+.catch(err=>{
+  console.log ('Error coonecting to the DB',err);
+})
+
